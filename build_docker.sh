bc22c6a9f1bb7fc57900b7dda0f551094fa4e43e
Carsten Koester <carsten@ckoester.net>
2020-03-04 15:17:17 -0800

    Make docker build process more platform independent
    Make Docker build process more portable. Rather than requiring golang
    on the build host, and rather than creating a build container that
    then needs to get run and deleted in separate steps, create a
    multistage Dockerfile and a wrapper that builds images in a
    "one image per docker build" manner.
    
    This change also keeps the build process self-contained as far as
    file systems are concerned, ie. the image build process does not
    depend on files, and does not modify files outside the git repository.

--- build_docker.sh
+++ build_docker.sh
@@ -1,15 +1,20 @@
 #!/bin/sh -e
 
-echo 'Updating alpine base image'
-docker pull alpine:latest
+BUILD_TAG=$(git describe --tags)
+VERSION=$(git rev-parse --short=8 HEAD)
+if ! [ -z "$(git status --porcelain)" ]
+then
+  export VERSION="${VERSION}_dirty"
+fi
 
-echo 'Building DANM builder container'
-docker build --no-cache --tag=danm_builder:1.0 scm/build
-
-echo 'Running DANM build'
-docker run --rm --net=host --name=danm_build -v $GOPATH/bin:/usr/local/go/bin -v $GOPATH/src:/usr/local/go/src danm_builder:1.0
-
-echo 'Cleaning up DANM builder container'
-docker rmi -f danm_builder:1.0
-
-echo 'DANM libraries successfully built!'
+for a in netwatcher svcwatcher webhook danmbin
+do
+  echo Building: ${netwatcher}
+  docker build \
+    --build-arg BUILD_TAG=${BUILD_TAG} \
+    --build-arg VERSION=${VERSION} \
+    --tag ${a}:${VERSION} \
+    --target ${a} \
+    --file scm/build/Dockerfile \
+    .
+done
